a8f3d2a82f57ffbf7d6a11b78c00b6eeac6e1603
Sanush-BR <76110181+Sanush-BR@users.noreply.github.com>
2024-11-18 22:28:43 +0530

    Changed root main method | For deployment issues.

--- src/main/java/com/demo/project/ProjectApplication.java
+++ src/main/java/com/demo/project/ProjectApplication.java
@@ -1,11 +1,31 @@
+// package com.demo.project;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class ProjectApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(ProjectApplication.class, args);
+// 	}
+
+// }
 package com.demo.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectApplication {
-
+public class ProjectApplication extends SpringBootServletInitializer {
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProjectApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
